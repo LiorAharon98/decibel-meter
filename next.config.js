61437d3954ff889bb883b8dcb1071d21ff34c580
LiorAharon98 <lior.ah98@gmail.com>
2023-07-02 15:28:55 +0300

    added new features && improve perfomance

--- next.config.js
+++ next.config.js
@@ -1,18 +1,8 @@
 /** @type {import('next').NextConfig} */
-
-if (process.env.NODE_ENV === "development") {
-  require("dotenv").config();
-}
-if (process.env.NODE_ENV === "production") {
-  require("dotenv").config();
-}
+require("dotenv").config();
 
 const nextConfig = {
   reactStrictMode: true,
-
-  env: {
-    API_URL: process.env.API_URL,
-  },
 };
 
 module.exports = nextConfig;
